2c536b2617a70525940156f6b3258768a56fd9bd
Sumit Mathur <152469505+Sumitmathur12@users.noreply.github.com>
2024-09-16 11:33:56 +0000

    DATATYPES AND VARIABLES

---
+++ 01_Basics/1variable.js
@@ -0,0 +1,11 @@
+const accountId = 1345667
+let accountEmail = "Smteu@gmail.com"
+var accountPassword = "2354"
+
+accountCity = "jaipur"
+//accountId= 2344 NOt allowed 
+console.log(accountId);
+accountEmail= "ertu@gmail.com"
+accountPassword= "ERjpw"
+accountCity= "benguluru"
+console.table([accountEmail, accountPassword,accountCity, accountId])
---
+++ 01_Basics/2Datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+// alert(3+4)  WE ARE USING NODEJS NOT BROWSER
+console.log(3+3) //code redeability should be high 
+console.log("sumit");
+let name = "sumit"
+let age = 19
+let isloogedin = true
+
+// Number -> 2 to the power of 53
+// bigint 
+// String
+// Boolean => True/False
+// Null => Standalone Value
+// Undefined => Abhi value assign nhi hui hai 
+//Symbols => Unique
+//object
+console.log(typeof null); //OBJECT
+console.log(typeof undefined); //UNDEFINED
+console.log(typeof "hitesh");
+console.log(typeof age)
+
+
