ba6984dc78169a6f9012f00ec9a6949d94b16718
Sumit Mathur <152469505+Sumitmathur12@users.noreply.github.com>
2024-09-18 18:29:18 +0000

    conversion in number

---
+++ 01_Basics/03conversionOperations.js
@@ -0,0 +1,72 @@
+// let score = null;
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueinnumber = Number(score);
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
+
+// // "33"=> 33
+// // "33abc" => NaN
+// // true => 1 , false=> 0
+
+// let isloogedin = "sumit"
+
+// let booleanisloggedin = Boolean(isloogedin);
+// // console.log(booleanisloggedin)
+
+// // 1=> true ; 0 => false
+// // "" => false
+// // "Sumit" => 
+// let someNumber = 33;
+// let stringNumber = string(someNumber);
+// console.log(stringNumber);
+// let score = null;
+// console.log(typeof score); // object
+// console.log(typeof(score)); // object
+
+// let valueinnumber = Number(score);
+// console.log(typeof valueinnumber); // number
+// console.log(valueinnumber); // 0
+
+// let isloogedin = "sumit";
+// let booleanisloggedin = Boolean(isloogedin);
+// // console.log(booleanisloggedin);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+// console.log(typeof stringNumber); // "33"
+
+
+// ************************OPERATIONS*********************** 
+
+let value = 33 
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+// let str1 = "hitesh"
+// let str2 = "sumit"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("2" + 2 + 4);  // MAKE ALL STRING BECAUSE FIRST VALUE IS STRING 
+// console.log(2 + 3 + "4")  // FIRST CALCULATE WITHOUT STRING VALUE AND THEN CONTACTINATED WITH STRING 
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2+2
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
+
+
