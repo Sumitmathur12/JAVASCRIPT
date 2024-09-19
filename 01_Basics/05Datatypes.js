41d725592f93117ccf3613648ca2cfa4753d3d7d
Sumit Mathur <152469505+Sumitmathur12@users.noreply.github.com>
2024-09-19 02:08:11 +0000

    Datatype-Summary

---
+++ 01_Basics/05Datatypes.js
@@ -0,0 +1,35 @@
+// // PRIMITIVE DATA TYPES
+// // IS OF 7 TYPES : STRING , NUMBER , BOOLEAN , NULL ,  UNDEFINED , SYMBOL , BIGINT
+
+// const score  = 100
+// const scorevalue = 100.3
+
+// const isLoggedIn = false
+const outsidetemp = null 
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherid = Symbol("123")
+
+// console.log(id === anotherid)
+// const bigNumber = 2155545562362n
+
+// Reference (Non- Primitive)
+//  ARRAYS , OBJECTS , FUNCTION 
+
+let myobj= {
+    name : "Sumit",
+    age: 19,
+}
+
+const Heroes = ["Shakitaman" , "natraj" , "Doga" ];
+
+const MyFuction = function(){
+    console.log("Hello world")
+
+}
+console.log(typeof outsidetemp)
+console.log(typeof scorevalue)
+console.log(typeof MyFuction)
+console.log(typeof Heroes)
+console.log(typeof myobj)
