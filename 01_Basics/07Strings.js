f62605849a326aebebcaae3cd7f5d0da6ee88d7c
Sumit Mathur <152469505+Sumitmathur12@users.noreply.github.com>
2024-09-19 04:56:26 +0000

    strings in JS

---
+++ 01_Basics/07Strings.js
@@ -0,0 +1,30 @@
+const name = "sumit"
+const repoCount = 50
+// console.log(name + repoCount  + " value")
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const Gamename= new String("Sumit-hhc-mathur")
+// console.log(Gamename[0]);               //GIVE CHAR AT GIVEN POSITION 
+// console.log(Gamename.__proto__);
+// console.log(Gamename.length);           // GIVE LENGTH OF YOUR GVEN STRING 
+// console.log(Gamename.toUpperCase());     // MAKE STRING TO UPPERCASE
+// console.log(Gamename.charAt(6))          // GIVE CHARACTER AT ANY POSITION 
+// console.log(Gamename.indexOf("t"))       // GIVE INDEX OR POSITION OF CHAR
+
+// const Newstring = Gamename.substring(0,4)  // YOu  CAN'T GIVE NEGATIVE VALUE IF YOU GIVE , IGNORED AND START AT ZERO
+// console.log(Newstring)
+
+// const Anotherstring = Gamename.slice(-7,4) // YOU CAN GIVE NEGATIVE VALUE TOO 
+// console.log(Anotherstring)
+
+// const NewStringone = " Sumit  "
+// console.log(NewStringone)
+// console.log(NewStringone.trim())  // trim keyword remove all the extra space in string 
+
+// const url = "https://Sumit.com/mathur123%20mathur";
+// console.log(url.replace("%20" , "-"))
+// console.log(url.includes("Sundar"));
+// console.log(url.includes("Sumit"));
+
+
+console.log(Gamename.split("-"))
