3176bc7ec82951d8d3bd12e83d29bfab74ff06b9
Sumit Mathur <152469505+Sumitmathur12@users.noreply.github.com>
2024-09-23 10:43:46 +0000

    if-else-if condition

---
+++ 04_Day15/condionalF.js
@@ -0,0 +1,27 @@
+// //Conditonal statement
+// console.log("Before my if statement");
+// let age = 23;
+// if(age>13){
+//     console.log("Print you are verified") 
+//     let a = 5;
+//     console.log(a*5);                                            //// if this if is true then it will ecxute this code otherwise not 
+// }
+// if(age< 18){
+//     console.log("You can not give vote")
+// }
+// console.log("After my if statement");
+
+
+//CREATE A TRAFFIC LIGHT SYSTEM THAT SHOWS WHAT TO DO BASED ON COLOR.
+
+let color = "red";
+//Traffic light colors 
+if(color == "red"){
+    console.log("stop")
+}
+if(color == "yellow"){
+    console.log("slow down")
+}
+if(color == "green"){
+    console.log("go")
+}
