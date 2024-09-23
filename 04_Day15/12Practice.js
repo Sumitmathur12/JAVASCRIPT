0c357c956babd511da94a3979df159db6d5a7b44
Sumit Mathur <152469505+Sumitmathur12@users.noreply.github.com>
2024-09-23 11:14:17 +0000

    Practice question

---
+++ 04_Day15/12Practice.js
@@ -0,0 +1,15 @@
+//CREATE A SYSTEM TO CALCULATE POPCORN PRICE BASED ON THE SIZE CUSTOMER ASKED 
+let size = "XL"
+if(size === "XL"){
+    console.log("the price will be Rs 250");
+    
+}
+else if(size === "L"){
+    console.log("price will be Rs 200");
+    
+}
+else if (size === "M"){
+    console.log("Price will be Rs 100");
+    
+}
+
