3c56e0b1854a3984c6b80728eb10f7f1820225d5
Sumit Mathur <152469505+Sumitmathur12@users.noreply.github.com>
2024-09-23 11:34:59 +0000

    TRUTHY&FALSY

---
+++ 04_Day15/16Truthy&Falsy.js
@@ -0,0 +1,20 @@
+// if(true){
+//     console.log("Print what you need");
+    
+// }
+// else{
+//     console.log("what you print");
+    
+// }
+if(false){
+    console.log("Print what you need");
+    
+}
+else{
+    console.log("what you print");
+    
+}
+
+//FALSY VALUE CONTAIN ELEMENTS => FALSE , 0 , -0 , ON(BRIGHT VALUE) , EMPTY STRING ("") , NULL , UNDEFINED , NaN 
+
+// TRUTHY VALUE => EVERYTHING ELSE
