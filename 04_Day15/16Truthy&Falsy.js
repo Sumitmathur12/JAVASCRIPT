d537ef947f89d5926401b9eaa0321468b6a5211c
Sumit Mathur <152469505+Sumitmathur12@users.noreply.github.com>
2024-09-24 04:33:11 +0000

    Truthy&Falsy

--- 04_Day15/16Truthy&Falsy.js
+++ 04_Day15/16Truthy&Falsy.js
@@ -18,3 +18,6 @@ else{
 //FALSY VALUE CONTAIN ELEMENTS => FALSE , 0 , -0 , ON(BRIGHT VALUE) , EMPTY STRING ("") , NULL , UNDEFINED , NaN 
 
 // TRUTHY VALUE => EVERYTHING ELSE
+
+let firstname= prompt("Enter your name");
+console.log("firstname");
