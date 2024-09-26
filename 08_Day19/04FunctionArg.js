1860a5a66351d4db27edd57f294c66d268da1cb4
Sumit Mathur <152469505+Sumitmathur12@users.noreply.github.com>
2024-09-26 19:24:39 +0000

    Function Arguements

---
+++ 08_Day19/04FunctionArg.js
@@ -0,0 +1,20 @@
+// function PrintName(name , age){
+//     console.log(` ${name}'s age is ${age}`);
+// }
+// PrintName("Sumit " , 20)
+// PrintName("amit " , 20)
+// PrintName("anip[] " , 20)
+
+// function sum(a , b){
+//     console.log(a + b);
+// }
+// sum(2 , 5)
+// sum(4,6 )
+// sum(8 , 4)
+
+function Multiplication(n){
+    for(let i = n ; i<= n*10 ; i= i+n){
+        console.log(i)
+    }
+}
+Multiplication(8)
